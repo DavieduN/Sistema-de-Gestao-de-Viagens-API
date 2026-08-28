1fd26e19f0116b982e8e79fb7bc5cafddb34ed0e
Dyogo Bendo <dyogoromagnabendo@gmail.com>
2026-08-28 20:50:34 -0300

    feat: listar viagem

--- src/main/java/unioeste/com/br/gestaoviagem/viagem/controller/ViagemController.java
+++ src/main/java/unioeste/com/br/gestaoviagem/viagem/controller/ViagemController.java
@@ -4,15 +4,14 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import unioeste.com.br.gestaoviagem.empregado.domain.Empregado;
 import unioeste.com.br.gestaoviagem.empregado.service.EmpregadoService;
 import unioeste.com.br.gestaoviagem.viagem.domain.Viagem;
 import unioeste.com.br.gestaoviagem.viagem.domain.ViagemForm;
 import unioeste.com.br.gestaoviagem.viagem.service.ViagemService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/viagem")
@@ -26,5 +25,17 @@ public class ViagemController {
         Empregado empregado = empregadoService.buscarPorMatricula(viagemForm.getEmpregadoMatricula());
         Viagem novaViagem = viagemService.criar(viagemForm, empregado);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaViagem);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Viagem>> listarTodas() {
+        List<Viagem> viagens = viagemService.listarTodas();
+        return ResponseEntity.ok(viagens);
+    }
+
+    @GetMapping("/{numero}")
+    public ResponseEntity<Viagem> buscarPorId(@PathVariable Long numero) {
+        Viagem viagem = viagemService.buscarPorId(numero);
+        return ResponseEntity.ok(viagem);
     }
 }
--- src/main/java/unioeste/com/br/gestaoviagem/viagem/service/ViagemService.java
+++ src/main/java/unioeste/com/br/gestaoviagem/viagem/service/ViagemService.java
@@ -1,11 +1,15 @@
 package unioeste.com.br.gestaoviagem.viagem.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import unioeste.com.br.gestaoviagem.empregado.domain.Empregado;
 import unioeste.com.br.gestaoviagem.viagem.domain.Viagem;
 import unioeste.com.br.gestaoviagem.viagem.domain.ViagemForm;
 import unioeste.com.br.gestaoviagem.viagem.repository.ViagemRepository;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -26,5 +30,16 @@ public class ViagemService {
         viagem.setEmpregado(empregado);
 
         return viagemRepository.save(viagem);
+    }
+
+    public List<Viagem> listarTodas() {
+        return viagemRepository.findAll();
+    }
+
+    public Viagem buscarPorId(Long numero) {
+        return viagemRepository.findById(numero)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Viagem de número " + numero + " não encontrada."
+                ));
     }
 }
